781831cf97ad688962c1d50cee7cddac5e1d8a6f
Amber Sinha <114065662+Ambersinha97@users.noreply.github.com>
2024-06-30 20:38:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function firstChar(text) {
   let str=""
 	for(let i=0;i<text.length;i++){
 	  if(text.charAt(i)!=" ")
-		  str=text.charAt(i)
+		  return text.charAt(i)
   }
 	return str
 }
 
 // Do not change the code below
 
-//const text = prompt("Enter text:");
+// const text = prompt("Enter text:");
 alert(firstChar(text));
